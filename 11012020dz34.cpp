4d62ab093d737169b721199c4276e6e18dedd263
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:45:24 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz34.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры размер в дюймах.Перевести его в сантиметры.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите размер в дюймах : \n";
+	int a;
+	cin >> a;
+	cout << "см. = " << a * 2.54 << endl;
+	return 0;
+}
